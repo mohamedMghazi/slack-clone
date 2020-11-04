14fcb2353f2eff44310761e7e9521bf3fb302fd9
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-11-04 14:44:46 +0200

    Optimizations

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -29,7 +29,7 @@ export default () => {
 
   }, [roomId])
 
-  console.log(roomMessages);
+  console.log(roomDetails);
 
   return (
     <div className="chat">
--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -2,28 +2,35 @@ import React, {useState} from "react";
 import db from "../firebase/firebase";
 import {useStateValue} from "./StateProvider";
 import firebase from "firebase";
+import { animateScroll as scroll } from "react-scroll";
+//
+import "./style/chatInput.css";
 
 export default ({ channelName, channelId }) => {
     const [ input, setInput ] = useState('');
     const [{ user }] = useStateValue();
-    console.log(channelId)
+
+
     const sendMessage = (e) => {
         e.preventDefault();
-
         if(channelId) {
-            console.log(">>", channelId)
             db.collection('rooms').doc(channelId).collection('messages').add({
                 message: input,
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                 user: user.displayName,
                 userImage: user.photoURL
-            }).then(r => console.log(r)).catch((err) => console.log(err))
+            }).catch((err) => console.log(err))
         }
+        setInput("");
+        scroll.scrollToBottom();
     }
+
     return <div className="chatInput">
         <form>
-            <input value={input}
-                   onChange={(e) => setInput(e.target.value)} placeholder={`Message #${channelName?.toLowerCase()}`}/>
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)} placeholder={`Message #${channelName?.toLowerCase()}`}
+            />
             <button type="submit" onClick={(e) => sendMessage(e)}>SEND</button>
         </form>
     </div>
